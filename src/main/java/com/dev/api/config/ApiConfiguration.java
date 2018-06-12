c51466d165e508f1d0df1de5f67a2ed8ed7fdcbc
CC <CC@CC>
2018-06-13 00:31:54 +0800

    提交打包

--- src/main/java/com/dev/api/config/ApiConfiguration.java
+++ src/main/java/com/dev/api/config/ApiConfiguration.java
@@ -2,6 +2,8 @@ package com.dev.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -14,8 +16,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
-public class ApiConfiguration {
+public class ApiConfiguration extends WebMvcConfigurationSupport{
 
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("swagger-ui.html")
+              .addResourceLocations("classpath:/META-INF/resources/");
+
+      registry.addResourceHandler("/webjars/**")
+              .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
+	
 	@Bean
 	public Docket createRestApi() {
 		//swagger-ui-layer 
