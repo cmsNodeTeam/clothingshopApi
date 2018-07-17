18dc8fe7fe4ec7aa9dc5184ab9d0932fbe76e59f
CC <CC@CC>
2018-07-17 21:34:18 +0800

    1

--- src/main/resources/static/js/super/select2/select2Obj.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by CC on 2018/1/15.
- */
-define(['../../../select2/select2.full.min'
-    ,'../../../select2/select2-locale-all'],function (S,L) {
-    'use strict';
-    var e = jQuery.fn.select2.amd;
-    e.define('select2/i18n/all', [], L);
-});
--- src/main/resources/static/js/user/login/userLogin.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by CC on 2016/10/14.
- * 用户登录时的js
- */
-'use strict';
