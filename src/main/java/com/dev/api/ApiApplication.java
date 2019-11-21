ddc9be29e3be13111e5c02ceed869e206e93e34e
chaney <954275226@qq.com>
2019-11-21 23:21:43 +0800

    .

--- src/main/java/com/dev/api/ApiApplication.java
+++ src/main/java/com/dev/api/ApiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-		//ts  44f
+		//ts  44f,
 	}
 }
