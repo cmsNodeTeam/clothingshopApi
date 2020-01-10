ac18915e5e1fe8bb7b1cd4377c67a8ffcd9a50eb
Clannader <oliver..wu@shijigroup.com>
2020-01-10 23:13:38 +0800

    11

--- src/main/java/com/dev/api/config/CodeEnum.java
+++ src/main/java/com/dev/api/config/CodeEnum.java
@@ -2,7 +2,7 @@ package com.dev.api.config;
 
 public enum CodeEnum {
 	ERROR_404(404, "地址不存在"),
-	ERROR_500(999, "服务器报错 "),
+	ERROR_500(999, "服务器报错"),
 	METHOD_NOT_SUPPORTED(998, "请求方式不支持"),
 	ERROR_NULL(997, "服务器报空指针异常");
 	
